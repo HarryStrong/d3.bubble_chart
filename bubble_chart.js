d7182dfbbb0348c3b9a8c9e8a232422186db9de6
jpiraneo <jrpiraneo@gmail.com>
2016-01-31 17:17:06 -0800

    Update bubble_chart.js

--- bubble_chart.js
+++ bubble_chart.js
@@ -14,12 +14,12 @@ var tooltip = d3.select("body").append("div")
 .attr("class", "tooltip")
 .style("opacity", 0);
 
+
+var data = d3.csv.parse( d3.select("pre#data").text() );
 //this just grabs the text from the preformatted block
 //and parses it as if it was a csv file
 //in your real code, you would use d3.csv(filename, callbackFunction) 
 //and the rest would be inside your callback function:
-var data = d3.csv.parse( d3.select("pre#data").text() );
-
 
   data.forEach(function(d) {
     d.Team = d.Team;
@@ -46,6 +46,9 @@ var data = d3.csv.parse( d3.select("pre#data").text() );
     d.O9ZFO = +d.OZFO;
   });
 
+
+  //WE MAY NEED TO MOVE THE X SCALE OVER PADDING(30) UNITS BECAUSE WE TRANSFORMED THE GROUPED TAG OVER 30 UNITS TO ADD SOME MARGIN
+
   var x = d3.scale.linear()
   .domain([d3.min(data, function(d) { return d.GFPerc; }), d3.max(data, function(d) { return d.GFPerc; })])
   .range([padding, w - padding * 2]);
@@ -56,11 +59,9 @@ var data = d3.csv.parse( d3.select("pre#data").text() );
   .domain([d3.min(data, function(d) { return d.CFPerc;}), d3.max(data, function(d) { return d.CFPerc; })])
   .range([h - padding, padding]);
 
-
   var r = d3.scale.linear()
   .domain([95,105])
   .range([2,10]);
-
 
   var xAxis = d3.svg.axis()
   .scale(x)
@@ -71,16 +72,14 @@ var data = d3.csv.parse( d3.select("pre#data").text() );
   .scale(y)
   .orient("left")
   //.ticks(10);
-
-
-
+  
   svg.append("g")
     .attr("class", "x axis")
     .attr("transform", "translate(0," + (h - padding) + ")")
     .call(xAxis)
     .append("text")
     .style("text-anchor", "end")
-    .attr("x", w - padding * 2)
+    .attr("x", w - padding * 2 - 5)
     .attr("y", -6)
     .text("GF%")
 
@@ -91,33 +90,79 @@ var data = d3.csv.parse( d3.select("pre#data").text() );
     .append("text")
     .attr("transform", "rotate(-90)")
     .attr("y", 15)
-    .attr("x", -30)
+    .attr("x", -35)
     .attr("dy", ".21em")
     .style("text-anchor", "end")
     .text("CF%");
 
+console.log(d3.min(data, function(d) {
+	return d.CFPerc;
+}));
 
-  var circles = svg.selectAll("circles")
+console.log(d3.min(data, function(d) {
+	return d.GFPerc;
+}));
+
+
+	
+svg.selectAll(".h").data(d3.range(
+		d3.round(d3.min(data, function(d) { 
+    	return d.CFPerc; })) + 1, 
+    d3.round(d3.max(data, function(d) { 
+    	return d.CFPerc; })) + 1 ))
+  .enter()
+  .append("line").classed("h",1)
+  .attr("x1", padding).attr("x2", w - padding * 2)
+  .attr("y1",y).attr("y2",y);
+  
+svg.selectAll(".v").data(d3.range(
+		d3.round(d3.min(data, function(d) { 
+    	return d.GFPerc; })) + 1, 
+    d3.round(d3.max(data, function(d) { 
+    	return d.GFPerc; })) + 1 ))
+  .enter()
+  .append("line").classed("v",1)
+  .attr("y1",padding).attr("y2",h-padding)
+  .attr("x1",x).attr("x2",x);
+  
+
+
+
+var circles = svg.selectAll("circles")
   .data(data)
   .enter().append("circle")
-  .style("fill", "steelblue")
-  .transition()
-  .duration(300)
+    .attr("class", "circle")
+    .attr("cx", function(d) { return x(0); })
+    .attr("cy", function(d) { return y(0); })
+    .attr("r", function(d) { return r(0); })
+    .on("mouseover", function(d) {
+      tooltip.transition()
+        .duration(200)
+        .style("opacity", .8);
+      tooltip.html("<strong>" + d.Team + 
+      						"</strong><br>(GF%: " + d.GFPerc
+                   + ", CF%: " + d.CFPerc + ")")
+        .style("left", (d3.event.pageX + 10) + "px")
+        .style("top", (d3.event.pageY - 28) + "px");
+      circles.style("fill", "gray");
+      d3.select(this).style("fill", "red");
+    })
+    .on("mouseout", function(d) {
+      tooltip.transition()
+        .duration(500)
+        .style("opacity", 0);
+     	//circles.attr("class", "circle");
+      circles.style("fill", "steelblue");
+    });
+
+
+circles
+	.transition()
+  .delay(function(d,i) { return i * 50; })
+  .duration(1000)
   .attr("cx", function(d) { return x(d.GFPerc); })
   .attr("cy", function(d) { return y(d.CFPerc); })
   .attr("r", function(d) { return r(d.PDO); })
-  .style("opacity", .7)
-  .on("mouseover", function(d) {
-    tooltip.transition()
-      .duration(200)
-      .style("opacity", .9);
-    tooltip.html(d.Team + "<br/> (GF%: " + d.GFPerc
-                 + ", CF%: " + d.CFPerc + ")")
-      .style("left", (d3.event.pageX + 5) + "px")
-      .style("top", (d3.event.pageY - 28) + "px");
-  })
-  .on("mouseout", function(d) {
-    tooltip.transition()
-      .duration(500)
-      .style("opacity", 0);
-  });
+
+
+
